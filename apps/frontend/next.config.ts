f72423aac6febff7572287468075a80408fa5b23
wxrayut <warayut.tik2709@gmail.com>
2026-07-28 01:54:45 +0700

    chore(frontend): add Next.js configuration

---
+++ apps/frontend/next.config.ts
@@ -0,0 +1,23 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "token-icons.s3.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "coin-images.coingecko.com",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
